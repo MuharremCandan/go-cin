a0cf10ce0013f14add533d57de3a6a425924e8e3
MuharremCandan <1muharremcandan@gmail.com>
2024-02-27 21:03:30 +0300

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Gin works properly!",
+		})
+	})
+
+	r.Run(":8089")
+}
